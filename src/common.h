e07a8748d3d427d2ee464e6dcec4631fd3a912aa
Unlikely_Hacker <jlm13v@my.fsu.edu>
2018-11-01 16:21:04 -0400

    Better push

---
+++ src/common.h
@@ -0,0 +1,14 @@
+#include <stdint.h> //allows (u)int64_t
+#include <stdio.h>
+#include <stdlib.h>
+typedef unsigned __int128 uint128_t;
+
+/*
+ * CBC
+ * OFB
+ * CTR
+ * ECB
+ * RC4
+ * Hash/mac
+ * ECC
+ */
