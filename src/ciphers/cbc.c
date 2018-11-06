60edbe91975974fa59838a6563536de74df8a54b
Unlikely_Hacker <jlm13v@my.fsu.edu>
2018-11-06 14:28:43 -0500

    More module

--- src/ciphers/cbc.c
+++ src/ciphers/cbc.c
@@ -2,11 +2,18 @@
 //Just xor by previous cipher text
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
   FILE* ptr;
+  
   ptr = fopen("test","rb");  // r for read, b for binary
   fseek(ptr, 0L, SEEK_END);
-  unsigned long long IV = ftell(ptr); //gets size of file
+  unsigned long long filesize = ftell(ptr); //gets size of file
+  char *IV = (char*) malloc(sizeof(char) * (filesize + 1));
+  sprintf(IV,"%0*d\n", (int)filesize, 0);
+  printf("IV[%llu] is: %s\n",filesize,IV);
+
+  fclose(ptr);
   return 0;
 }
