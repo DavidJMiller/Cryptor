63acdf580a44edc39968fe59cf18413875907aa4
Joshua Bittel <joshuabittel123@gmail.com>
2018-11-08 12:51:44 -0500

    set up ecb for printing variable block size encryption to outputfile

--- src/ciphers/ecb.c
+++ src/ciphers/ecb.c
@@ -1,23 +1,42 @@
 //Written by: Joshua Bittel
 //Electronic Code Block
 
-//#include <common.h>
+#include <common.h>
 #include <stdio.h>
 
+//argv: 1 = block cipher type, block cipher size, input file name, output file name
+
 int main(int argc, char **argv){
-  char buffer[64];
+  if (argc < 5){
+    printf("Usage: ./ecb [blockCipherType] [blockCipherSize] [inputFile] [outputFile]\n");
+    exit(EXIT_FAILURE);
+  }
+
+  //TODO: do a switch based on blockCipherType
+
+  int blockCipherSize = atoi(argv[2]);
+  char * filein = argv[3];
+  char * fileout = argv[4];
+  
+  uint8_t buffer[blockCipherSize];
   FILE* file_open, * out_open;
-  file_open = fopen("test","rb");  // r for read, b for binary
-  out_open = fopen("out", "wb");
+  file_open = fopen(filein,"rb");  // r for read, b for binary
+  out_open = fopen(fileout, "wb");
   fseek(file_open, 0L, SEEK_END);
   rewind(file_open);
   unsigned long long file_size = ftell(file_open); //gets size of file
-  while(fread(buffer, 1, 64, file_open)){
+
+  //TODO: pad the file with correct block cipher size here
+  
+  while(fread(buffer, 1, blockCipherSize, file_open)){
     //call encryption function here
-    size_t count_write = fwrite(buffer, 1, 64, out_open);
-    if (count_write != 64){
+    size_t count_write = fwrite(buffer, 1, blockCipherSize, out_open);
+    if (count_write != blockCipherSize){
       perror("Write error\n");
     }
   }
+  fclose(file_open);
+  fclose(out_open);
+  
   return 0;
 }
