94a66eb50f0327187452fce50bedbfff9009a6f7
Unlikely_Hacker <jlm13v@my.fsu.edu>
2018-11-16 23:49:59 -0500

    cbc edits

--- src/ciphers/cbc.c
+++ src/ciphers/cbc.c
@@ -1,23 +1,51 @@
 //Written by: Jordan Mussman
 //Just xor by previous cipher text
 
-//args: usage_flag, input_file_name, output_file_name[, block_size, blockfunc(DES,AES)]
-//args: Usage: ./*.exe [blockCipherType] [blockCipherSize] [inputFile] [outputFile]
+//argv: 1 = usageFlag['e', 'd'], inputfile, output, file, --optional [blockfunction]
 
+#include "../common.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv){
-  FILE* ptr;
+  FILE *file_in, *file_out;
  //open 
-  ptr = fopen("test","rb");  // r for read, b for binary
- //pad - 1(0)*
-  fseek(ptr, 0L, SEEK_END);
-  unsigned long long filesize = ftell(ptr); //gets size of file
-  char *IV = (char*) malloc(sizeof(char) * (filesize + 1));
-  sprintf(IV,"%0*d\n", (int)filesize, 0);
-  printf("IV[%llu] is: %s\n",filesize,IV);
- //xor
-  fclose(ptr);
+  file_in = fopen(argv[2],"r");  // r for read, b for binary
+  file_out = fopen(argv[3],"w");
+  //pad - 1(0)*
+  fseek(file_in, 0L, SEEK_END);
+  unsigned long long filesize = ftell(file_in); //gets size of file
+  
+  //To Do: add padding
+
+  uint8_t IV[16] = { (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t)     0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00 };
+  uint8_t key[16] = { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
+  uint8_t *ctxt[16];
+  memcpy(ctxt, &IV,sizeof(ctxt));
+  uint8_t buf[16];
+  if(strcmp(argv[1], "e")==0){
+  //encryption
+      while(fread(buf,1,16,file_in)){
+        ctxt ^= buf; 
+        //make if/else_if for aes or des
+	AES_encrypt(buf, key, ctxt, 16);
+
+        if(fwrite(ctxt, 1, 16, file_out) != 16)
+          perror("Write error\n");
+      } 
+  }else if(strcmp(argv[1],"d")==0){
+  //decryption
+      while(fread(buf,1,16,file_in)){
+        ctxt ^= buf;
+        //make if/else_if for aes or des
+	AES_encrypt(buf, key, ctxt, 16);
+     
+        if(fwrite(ctxt, 1, 16, file_out) != 16)
+          perror("Write error\n");
+      }
+  }
+  fclose(file_out);
+  fclose(file_in);
   return 0;
 }
