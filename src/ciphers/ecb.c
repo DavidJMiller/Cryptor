a1c88fcdce4df58efa16e9a1ad485b36af98570b
Joshua Bittel <joshuabittel123@gmail.com>
2018-11-20 16:23:05 -0500

    Working encrypt and decryption of ECB

--- src/ciphers/ecb.c
+++ src/ciphers/ecb.c
@@ -7,6 +7,8 @@
 //argv: 1 = usageFlag['e', 'd'], inputfile, output, file, --optional [blockfunction]
 
 int main(int argc, char **argv){
+  char * blockfunc_name;
+
   if (argc < 4){
     printf("Usage: ./ecb [usage] [inputFile] [outputFile] <keysize> <blockfunction>\n");
     exit(EXIT_FAILURE);
@@ -19,11 +21,15 @@ int main(int argc, char **argv){
   int blockCipherSize;
   if (argc > 4)
     blockCipherSize = atoi(argv[4]);
+  if (argc > 5)
+    blockfunc_name = argv[5];
   char * filein = argv[2];
   char * fileout = argv[3];
-  char usage = argv[1];
+  char usage = *argv[1];
   uint8_t buffer[16];
   uint8_t ciphered[16];
+  memset(buffer, 1, 16*sizeof(buffer[0]));
+  memset(ciphered, 1, 16*sizeof(ciphered[0]));
   FILE* file_open, * out_open;
   file_open = fopen(filein,"r");  // r for read, b for binary
   out_open = fopen(fileout, "w");
@@ -34,21 +40,27 @@ int main(int argc, char **argv){
   //TODO: pad the file with correct block cipher size here
   
   while(fread(buffer, 1, 16, file_open)){
-    //call usage function here ie: encrypt, decrypt, sign, authenticate
-    AES_encrypt(buffer, key, ciphered, 16);
+    if (usage == 'e'){
+      //call usage function here ie: encrypt, decrypt, sign, authenticate
+      AES_encrypt(buffer, key, ciphered, 16);
+    }
+    else if (usage == 'd'){
+      AES_decrypt(buffer, key, ciphered, 16);
+    }
     int i;
-    for (i = 0; i < 16; ++i)
+    /*for (i = 0; i < 16; ++i)
       printf("%x", ciphered[i]);
-    printf("\n");
+      printf("\n");*/
     size_t count_write = fwrite(ciphered, 1, 16, out_open);
     if (count_write != 16){
       perror("Write error\n");
     }
-    
+    memset(buffer, 1, 16*sizeof(buffer[0]));
+    memset(ciphered, 1, 16*sizeof(ciphered[0]));
   }
-  rewind(out_open);
+  /*rewind(out_open);
   unsigned int jpg = 0xffd8ffdb;
-  fprintf(out_open, "%x", jpg);
+  fprintf(out_open, "%x", jpg);*/
   fclose(file_open);
   fclose(out_open);
   
