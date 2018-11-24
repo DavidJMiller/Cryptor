0c7fba5dad87d95269d6fe6c35c3f269c3cf2e4c
Unlikely_Hacker <jlm13v@my.fsu.edu>
2018-11-24 14:00:12 -0500

    cbc done

--- src/ciphers/cbc.c
+++ src/ciphers/cbc.c
@@ -21,13 +21,14 @@ int main(int argc, char **argv){
 
   uint8_t IV[16] = { (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t)     0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00, (uint8_t) 0x00 };
   uint8_t key[16] = { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
-  uint8_t *ctxt[16];
+  uint8_t ctxt[16];
   memcpy(ctxt, &IV,sizeof(ctxt));
   uint8_t buf[16];
   if(strcmp(argv[1], "e")==0){
   //encryption
       while(fread(buf,1,16,file_in)){
-        ctxt ^= buf; 
+        for(int i = 0; i < sizeof(buf);i++)
+	    ctxt[i] ^= buf[i]; 
         //make if/else_if for aes or des
 	AES_encrypt(buf, key, ctxt, 16);
 
@@ -37,9 +38,10 @@ int main(int argc, char **argv){
   }else if(strcmp(argv[1],"d")==0){
   //decryption
       while(fread(buf,1,16,file_in)){
-        ctxt ^= buf;
+        for(int i = 0; i < sizeof(buf);i++)
+	    ctxt[i] ^= buf[i]; 
         //make if/else_if for aes or des
-	AES_encrypt(buf, key, ctxt, 16);
+	AES_decrypt(buf, key, ctxt, 16);
      
         if(fwrite(ctxt, 1, 16, file_out) != 16)
           perror("Write error\n");
