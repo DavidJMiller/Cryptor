e343ce35d96ac95faf653d2b4319d161e626d1bd
DavidJMiller <dm11ad@my.fsu.edu>
2018-12-01 19:36:14 -0500

    some stuff

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -std=c11 -Wall -g -O0
 
 SOURCE = src/ciphers/
 
-all: ecb cbc ofb ctr
+all: ecb cbc ofb ctr rc4
 
 ecb: $(SOURCE)ecb.c src/blockfuncs/blockfuncs.c
 	gcc -g -O0 -Isrc/blockfuncs -I. -o $(SOURCE)ecb $(SOURCE)ecb.c src/blockfuncs/blockfuncs.c
@@ -17,8 +17,8 @@ cbc: $(SOURCE)cbc.c src/blockfuncs/blockfuncs.c
 ofb: $(SOURCE)ofb.c src/blockfuncs/blockfuncs.c
 	gcc -g -O0 -Isrc/blockfuncs -I. -o $(SOURCE)ofb $(SOURCE)ofb.c src/blockfuncs/blockfuncs.c
 
-rc4: $(SOURCE)Rc4.c
-	gcc -g -O0 -I. -o $(SOURCE)Rc4 $(SOURCE)Rc4.c
+rc4: $(SOURCE)RC4.c
+	gcc -g -O0 -I. -o $(SOURCE)RC4 $(SOURCE)RC4.c
 
 clean:
 	rm $(SOURCE)ecb $(SOURCE)ctr $(SOURCE)cbc $(SOURCE)ofb $(SOURCE)Rc4
--- src/ciphers/RC4.c
+++ src/ciphers/RC4.c
@@ -3,20 +3,23 @@
 #include <string.h>
 #include<stdlib.h>
 
-char key[256] = "Key";
-int keyLen = 3;
+uint8_t key[] = { 0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b,  
+                     0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x11, 0x1f,  
+                     0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98,  
+                     0x10, 0xa3, 0x09, 0x14, 0xdf};
+int keyLen = 256;
 
 /* key scheduler algorithm */
-void keySchedule(unsigned char *S)
+void keySchedule(uint8_t S[256])
 {
-    int i, j;
-    unsigned char tmp;
+    uint8_t i, j;
+    uint8_t tmp;
 
-    for(i = 0; i < 255; i++)
+    for(i = 0; i < 256; i++)
         S[i] = i;
 
     j = 0;
-    for(i = 0; i < 255; i++)
+    for(i = 0; i < 256; i++)
     {
         j = (j + S[i] + key[i % keyLen]) % 256;
         tmp = S[i];
@@ -27,32 +30,93 @@ void keySchedule(unsigned char *S)
     return;
 }
 
-void PRGA(unsigned char *S, char *p, unsigned char *c)
+void PRGA(uint8_t S[256], FILE *srcFile, FILE *destFile)
 {
-    int i, j;
-    unsigned char tmp;
+    uint8_t i, j;
+    uint8_t tmp, *c, *buffer;
 
     i = 0;
     j = 0;
-    for(i = 0; i < strlen(p); i++)
-    {
+    while(fread(buffer, 1, 1, srcFile))                                       
+    {                                                                           
         i = (i + 1) % 256;
         j = (j + S[i]) % 256;
         tmp = S[i];
         S[i] = S[j];
         S[j] = tmp;
-        c[i] = S[(S[i] + S[j]) % 256];
+        *c = S[(S[i] + S[j]) % 256];
+        fwrite(c, 1, 1, destFile);
     }
-
-    return;
 }
 
 int main(int argc, char **argv)
 {
     if(argc < 2)
-        return -1;
+    {
+        printf("Incorrect usage\n");
+        exit(EXIT_FAILURE);
+    }
 
-    unsigned char *ciphertext = malloc(sizeof(int) * strlen(argv[1]));
-    keySchedule(ciphertext);
-    printf("%s\n", ciphertext);
+    FILE *fp;
+    char title[255];
+    char value[255];
+    char action;
+    char filein[255];
+    char blockfunc[255];
+    int func_index;
+
+    fp = fopen(argv[1], "r");
+    while(fscanf(fp, "%s %s", title, value) != EOF)
+    {
+        if(!strcmp(title, "action"))
+        {
+            action = value[0];
+        }
+        else if(!strcmp(title, "blockfunc"))
+        {
+            strcpy(blockfunc, value);
+        }
+        else if(!strcmp(title, "keylen"))
+        {
+            keyLen = atoi(value);
+        }
+        else if(!strcmp(title, "keylen"))
+        {
+            strcpy(filein, value);
+        }
+    }
+
+    if(!strcmp(blockfunc, "aes"))
+    {
+        func_index = 0;
+    }
+    else if(!strcmp(blockfunc, "test1"))
+    {
+        func_index = 0;
+    }
+    else if(!strcmp(blockfunc, "test2"))
+    {
+        func_index = 0;
+    }
+
+    char fileout[255];
+    if (action == 'e')
+    {
+        snprintf(fileout, sizeof(fileout), "%s.encrypt", filein);
+    } 
+    if (action == 'd')
+    {
+        snprintf(fileout, sizeof(fileout), "%s.decrypt", filein);
+    }
+    FILE *file_open, *out_open;
+    uint8_t cipher;
+    uint8_t S[256];
+    file_open = fopen(filein,"r"); 
+    out_open = fopen(fileout, "w");
+
+    keySchedule(S);
+    PRGA(S, file_open, out_open);
+
+    fclose(file_open);
+    fclose(out_open);
 }
